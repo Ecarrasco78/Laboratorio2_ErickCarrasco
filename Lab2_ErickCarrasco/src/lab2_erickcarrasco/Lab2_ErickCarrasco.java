5f0cea80ea58ca582527ca49a6aca08163d4028f
Ecarrasco <ecarrasco78@unitec.com>
2018-05-04 14:02:10 -0600

    Se ha creado un menu

---
+++ Lab2_ErickCarrasco/src/lab2_erickcarrasco/Lab2_ErickCarrasco.java
@@ -0,0 +1,26 @@
+package lab2_erickcarrasco;
+import java.util.*;
+
+
+public class Lab2_ErickCarrasco {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Welcome");
+        int opt;
+        ArrayList users = new ArrayList();
+        ArrayList grades = new ArrayList();
+        ArrayList tests = new ArrayList();
+        System.out.println("1/ Admin");
+        System.out.println("2/ Create account");
+        System.out.println("3/ Log in");
+        System.out.println("4/ Shut Down");
+        System.out.println(" ");
+        opt = read.nextInt();
+        switch (opt){
+            case 1:
+                
+        }
+    }
+    
+}
